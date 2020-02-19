291473af393f3bba92beef23b9194c61fc487d6f
lidiambsouza <lidiambsouza@gmail.com>
2020-02-19 18:35:56 -0300

    desenvolvendo o login

--- src/app/formalizacao/formalizacao.module.ts
+++ src/app/formalizacao/formalizacao.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TableModule} from 'primeng/table';
-import {DialogModule} from 'primeng/dialog';
-import {InputTextModule} from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 import { FormalizacaoComponent } from './formalizacao';
-import {FormalizacaoService} from './_services/formalizacao.service';
+import { FormalizacaoService } from './_services/formalizacao.service';
 import { HomeComponent } from './home/home.component';
 
 
--- src/app/formalizacao/home/home.component.ts
+++ src/app/formalizacao/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService, AuthenticationService } from '@app/_services';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  users: User[];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.loading = false;
+      this.users = users;
+    });
   }
 
 }
